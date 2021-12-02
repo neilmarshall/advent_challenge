2af4aa479921036c6d0a1005f044469ee57e6a5c
Neil Marshall <MarshalN@moodys.com>
2021-12-02 09:09:00 +0000

    add puzzle 1, 2022

---
+++ 2022/001/Program.fs
@@ -0,0 +1,14 @@
+﻿let calorieTotaller (calorieTotals : ResizeArray<int>, currentTotal) element =
+    match element with
+    | "" -> calorieTotals.Add(currentTotal); calorieTotals, 0
+    | _ -> calorieTotals, currentTotal + int element
+let calorieTotals =
+    System.IO.File.ReadLines("input.txt")
+    |> Seq.fold calorieTotaller (ResizeArray<int>(), 0)
+    |> fst
+    |> Seq.sortDescending
+    |> Seq.take 3
+
+printfn "Answer to Part One: %d" <| Seq.max calorieTotals  // should be 71471
+
+printfn "Answer to Part Two: %d" <| Seq.sum calorieTotals  // should be 211189
