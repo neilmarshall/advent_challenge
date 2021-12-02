aca912f91e805c755542ee51453953bf81e3a7bc
Neil Marshall <MarshalN@moodys.com>
2021-12-02 09:09:00 +0000

    add puzzle 1, 2021

---
+++ 2021/puzzle_1.py
@@ -0,0 +1,23 @@
+def count_increases(measurements):
+    """
+    >>> count_increases([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
+    7
+    """
+    return sum(1 for fst, snd in zip(measurements, measurements[1:]) if snd > fst)
+
+
+def count_window_increases(measurements):
+    """
+    >>count_window_increases([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
+    5
+    """
+    windows = [sum(measurements[i:i+3]) for i in range(len(measurements) - 2)]
+    return sum(1 for fst, snd in zip(windows, windows[1:]) if snd > fst)
+
+
+if __name__ == "__main__":
+    import doctest; doctest.testmod()
+    with open("input\puzzle_1.txt") as f:
+        measurements = list(map(int, f.readlines()))
+        print(f"Solution to Puzzle 1, Part A: {count_increases(measurements)}")
+        print(f"Solution to Puzzle 1, Part B: {count_window_increases(measurements)}")
