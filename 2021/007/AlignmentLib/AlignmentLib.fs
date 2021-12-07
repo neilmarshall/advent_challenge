636d25a6407f168c2142f32c3193c93a1cab7280
Neil Marshall <MarshalN@moodys.com>
2021-12-07 11:08:28 +0000

    add puzzle 7, 2021

---
+++ 2021/007/AlignmentLib/AlignmentLib.fs
@@ -0,0 +1,8 @@
+﻿module AlignmentLib
+
+let AlignBasic (input : int[]) : int =
+    {1..(Array.length input)} |> Seq.map (fun n -> input |> Array.map (fun e -> abs(e - n)) |> Seq.sum) |> Seq.min
+
+let AlignAdvanced (input : int[]) : int =
+    let T n = n * (n + 1) / 2
+    {1..(Array.length input)} |> Seq.map (fun n -> input |> Array.map (fun e -> T(abs(e - n))) |> Seq.sum) |> Seq.min
