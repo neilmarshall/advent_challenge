a6e1bc645e2d7949745d4e2ef9b9f28c46697f73
neilmarshall <neil.marshall@dunelm.org.uk>
2022-12-03 14:03:17 +0000

    add puzzle 3, 2022

---
+++ 2022/003/Program.fs
@@ -0,0 +1,20 @@
+﻿let input = System.IO.File.ReadLines("input.txt")
+
+let getDuplicatedElement (row : string) =
+    let compartmentA, compartmentB = Set.ofSeq row[.. row.Length / 2 - 1], Set.ofSeq row[row.Length / 2 ..]
+    Set.intersect compartmentA compartmentB |> Seq.exactlyOne
+
+let scoreElement e = int(e) - if System.Char.IsLower(e) then 96 else 38
+
+input
+|> Seq.map getDuplicatedElement
+|> Seq.map scoreElement
+|> Seq.sum
+|> printfn "Answer to Part One: %d"  // expected output is 8088
+
+input
+|> Seq.map Set.ofSeq
+|> Seq.chunkBySize 3
+|> Seq.map (Set.intersectMany >> Seq.exactlyOne >> scoreElement)
+|> Seq.sum
+|> printfn "Answer to Part Two: %d"  // expected output is 2522
